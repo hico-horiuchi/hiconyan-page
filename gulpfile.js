f8fbb73fb43197c791314b8c2020d20dac3b34fd
Akihiko Horiuchi <12ff5b8@gmail.com>
2024-09-01 21:24:40 +0900

    :art: Fix appearance

--- gulpfile.js
+++ gulpfile.js
@@ -1,69 +1,69 @@
-const { src, dest, parallel, watch } = require('gulp');
-const concat = require('gulp-concat');
-const connect = require('gulp-connect');
-const csso = require('gulp-csso');
-const fs = require('fs');
-const sass = require('gulp-sass')(require('sass'));
-const slm = require('gulp-slm');
-const yaml = require('js-yaml');
+const { src, dest, parallel, watch } = require("gulp");
+const concat = require("gulp-concat");
+const connect = require("gulp-connect");
+const csso = require("gulp-csso");
+const fs = require("fs");
+const sass = require("gulp-sass")(require("sass"));
+const slm = require("gulp-slm");
+const yaml = require("js-yaml");
 
 const config = new Map([
-  ['data', new Map([['path', 'src/yaml/data.yaml']])],
+  ["data", new Map([["path", "src/yaml/data.yaml"]])],
   [
-    'sass',
+    "sass",
     new Map([
       [
-        'options',
+        "options",
         {
-          includePaths: './node_modules/',
+          includePaths: "./node_modules/",
         },
       ],
     ]),
   ],
   [
-    'src',
+    "src",
     new Map([
-      ['scss', 'src/scss/*.scss'],
-      ['slm', 'src/slm/*.slm'],
-      ['yaml', 'src/yaml/**/*.yaml'],
+      ["scss", "src/scss/*.scss"],
+      ["slm", "src/slm/*.slm"],
+      ["yaml", "src/yaml/*.yaml"],
     ]),
   ],
   [
-    'dest',
+    "dest",
     new Map([
-      ['css', 'public'],
-      ['html', 'public'],
+      ["css", "public"],
+      ["html", "public"],
     ]),
   ],
 ]);
 
 css = () => {
-  return src(config.get('src').get('scss'))
-    .pipe(sass(config.get('sass').get('options')))
-    .pipe(concat('app.min.css'))
+  return src(config.get("src").get("scss"))
+    .pipe(sass(config.get("sass").get("options")))
+    .pipe(concat("app.min.css"))
     .pipe(csso())
-    .pipe(dest(config.get('dest').get('css')));
+    .pipe(dest(config.get("dest").get("css")));
 };
 
 html = () => {
-  let data = yaml.load(fs.readFileSync(config.get('data').get('path'), 'utf8'));
+  let data = yaml.load(fs.readFileSync(config.get("data").get("path"), "utf8"));
 
   data.copyright = new Date().getFullYear();
 
-  return src(config.get('src').get('slm'))
+  return src(config.get("src").get("slm"))
     .pipe(slm({ locals: data }))
-    .pipe(dest(config.get('dest').get('html')));
+    .pipe(dest(config.get("dest").get("html")));
 };
 
 preview = () => {
-  watch(config.get('src').get('scss'), css);
-  watch([config.get('src').get('slm'), config.get('src').get('yaml')], html);
+  watch(config.get("src").get("scss"), css);
+  watch([config.get("src").get("slm"), config.get("src").get("yaml")], html);
 };
 
 server = () => {
   connect.server({
     livereload: true,
-    root: 'public',
+    root: "public",
   });
 };
 
