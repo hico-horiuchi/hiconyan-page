c482460961bcf1267cb4ae6d47ca2326e776dee5
Akihiko Horiuchi <12ff5b8@gmail.com>
2025-01-27 19:24:34 +0900

    :jp: Add japanese version

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,29 @@
-process.env.SASS_PATH = "./node_modules"
+process.env.SASS_PATH = "./node_modules";
 
 const { src, dest, parallel, watch } = require("gulp");
 const concat = require("gulp-concat");
 const connect = require("gulp-connect");
 const csso = require("gulp-csso");
 const fs = require("fs");
+const rename = require("gulp-rename");
 const sass = require("gulp-sass")(require("sass"));
 const slm = require("gulp-slm");
 const yaml = require("js-yaml");
 
 const config = new Map([
-  ["data", new Map([["path", "src/yaml/data.yaml"]])],
+  [
+    "data",
+    new Map([
+      ["en", "src/yaml/en.yaml"],
+      ["ja", "src/yaml/ja.yaml"],
+    ]),
+  ],
   [
     "src",
     new Map([
       ["scss", "src/scss/*.scss"],
-      ["slm", "src/slm/*.slm"],
+      ["slm_index", "src/slm/index.slm"],
+      ["slm_page", "src/slm/page.slm"],
       ["yaml", "src/yaml/*.yaml"],
     ]),
   ],
@@ -36,19 +44,47 @@ css = () => {
     .pipe(dest(config.get("dest").get("css")));
 };
 
-html = () => {
-  let data = yaml.load(fs.readFileSync(config.get("data").get("path"), "utf8"));
+html_index = () => {
+  let data = {
+    copyright: new Date().getFullYear(),
+    en: yaml.load(fs.readFileSync(config.get("data").get("en"), "utf8")),
+    ja: yaml.load(fs.readFileSync(config.get("data").get("ja"), "utf8")),
+  };
+
+  return src(config.get("src").get("slm_index"))
+    .pipe(slm({ locals: data }))
+    .pipe(dest(config.get("dest").get("html")));
+};
+
+html_page_en = () => {
+  let data = yaml.load(fs.readFileSync(config.get("data").get("en"), "utf8"));
 
   data.copyright = new Date().getFullYear();
 
-  return src(config.get("src").get("slm"))
+  return src(config.get("src").get("slm_page"))
     .pipe(slm({ locals: data }))
+    .pipe(rename("en.html"))
+    .pipe(dest(config.get("dest").get("html")));
+};
+
+html_page_ja = () => {
+  let data = yaml.load(fs.readFileSync(config.get("data").get("ja"), "utf8"));
+
+  data.copyright = new Date().getFullYear();
+
+  return src(config.get("src").get("slm_page"))
+    .pipe(slm({ locals: data }))
+    .pipe(rename("ja.html"))
     .pipe(dest(config.get("dest").get("html")));
 };
 
 preview = () => {
   watch(config.get("src").get("scss"), css);
-  watch([config.get("src").get("slm"), config.get("src").get("yaml")], html);
+  watch(config.get("src").get("slm_index"), html_index);
+  watch(
+    [config.get("src").get("slm_page"), config.get("src").get("yaml")],
+    parallel(html_page_en, html_page_ja)
+  );
 };
 
 server = () => {
@@ -59,7 +95,7 @@ server = () => {
 };
 
 exports.css = css;
-exports.html = html;
+exports.html = parallel(html_index, html_page_en, html_page_ja);
 
-exports.default = parallel(css, html);
+exports.default = parallel(css, html_index, html_page_en, html_page_ja);
 exports.preview = parallel(preview, server);
